bdf93b68729c66623218e78f18e9cf9d3f6fcba1
Jhoan <jhoan-mac@hotmail.co.uk>
2013-06-14 22:53:24 -0500

    Now is possible to get theoretical and real means

--- RandomTimes.rb
+++ RandomTimes.rb
@@ -1,14 +1,17 @@
 class RandomTimes
-	def initialize(size,distro,params)
-		if distro == "Exponential" && params['lambda'] == nil
+	def initialize(size,distro,lambda)
+		if distro == "Exponential" && lambda == nil
 			raise "You need to provide a lambda parameter to generate Exponential or Poisson distributed values"
 		end
-		if (distro == "Poisson" || distro == "Exponential") && params['lambda'] < 0
+		if (distro == "Poisson" || distro == "Exponential") && lambda < 0
 			raise "Lambda should be greater than 0"
+		end
+		if (distro == "Poisson" && lambda % 1 != 0)
+			raise "Lambda should be an integer"
 		end
 		@distro = distro
 		@list = Array.new()
-		@params = params
+		@lambda = lambda
 		@size = size
 		generateValues()
 	end
@@ -18,11 +21,11 @@ class RandomTimes
 		case @distro
 			when "Exponential" 
 				@size.times do
-					@list.push(((-1 * Math.log( Random.rand() ) ) / @params['lambda'] * 100).floor / 100.0)
+					@list.push(((-1 * Math.log( Random.rand() ) ) / @lambda * 100).floor / 100.0)
 				end
 			when "Poisson"	
-				if @params['lambda'] < 30 #Small lambda, using Knoth algorithm
-					l = Math.exp(-1*@params['lambda'])
+				if @lambda < 30 #Small lambda, using Knoth algorithm
+					l = Math.exp(-1*@lambda)
 					@size.times do
 						k, p = 0 , 1
 						begin
@@ -31,11 +34,11 @@ class RandomTimes
 						end while p > l
 						@list.push(k-1)
 					end
-				else
-					c = 0.767 - 3.36/@params['lambda']
-					beta = Math::PI/Math.sqrt(3.0*@params['lambda'])
-					alpha = beta*@params['lambda']
-					k = Math.log(c) - @params['lambda'] - Math.log(beta)
+				else #Large Lambda, using Pa Algorithm
+					c = 0.767 - 3.36/@lambda
+					beta = Math::PI/Math.sqrt(3.0*@lambda)
+					alpha = beta*@lambda
+					k = Math.log(c) - @lambda - Math.log(beta)
 					@size.times do
 						begin
 							u = Random.rand()
@@ -47,7 +50,7 @@ class RandomTimes
 							v = Random.rand()
 							y = alpha - beta*x
 							lhs = y + Math.log(v/(1.0 + Math.exp(y))**2)
-							rhs = k + n*Math.log(@params['lambda']) - Math.lgamma(n+1)[0]
+							rhs = k + n*Math.log(@lambda) - Math.lgamma(n+1)[0]
 							if (lhs <= rhs)
 								@list.push(n)
 								break
@@ -63,19 +66,38 @@ class RandomTimes
 		end
 
 	end
+	def getTMean
+		case @distro 
+			when "Exponential"
+				@lambda**-1.to_f
+			when "Poisson"
+				@lambda
+			else
+				return 1/2
+		end
+	end
+	def getRMean
+		sum = 0
+		@list.each { |n| sum+=n }
+		sum/@size
+	end
 	def distro
 		@distro
 	end
 	def list
 		@list
 	end
+	def size
+		@size
+	end
 end
 #test
-params = { 'lambda' => 40}
-t = RandomTimes.new(5,"Poisson", params)
+t = RandomTimes.new(1000,"Poisson", 10)
 t.generateValues()
 File.open("test.txt", "w") do |file| 
 	t.list.each do |number|
 		file.puts number
 	end
 end
+puts t.getTMean
+puts t.getRMean
